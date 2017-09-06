26f0eb0053a656f6943220d95fedbab519076a13
Effluxation <patrick.dorosz@gmail.com>
2017-09-06 16:49:29 -0400

    Fix app.js to allow map rener

--- js/app.js
+++ js/app.js
@@ -25,12 +25,44 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function () {
+
+    // Player Sprite
+    this.sprite = 'images/char-horn-girl.png';
+    // this.x = ;
+    // this.y = ;
+};
+
+Player.prototype.update = function (dt) {
+    // body...
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    // body...
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+// var enemy1 = new Enemy();
+// allEnemies.push(enemy1);
 
+// var enemy2 = new Enemy();
+// allEnemies.push(enemy2);
+
+// var enemy3 = new Enemy();
+// allEnemies.push(enemy3);
+
+// var enemy4 = new Enemy();
+// allEnemies.push(enemy4);
+
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
